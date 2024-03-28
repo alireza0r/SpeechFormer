ed6e8bec27ffe7833b701d9173ece4374d9d542e
AlirezaRahmati <35702886+alireza0r@users.noreply.github.com>
2024-03-28 21:54:02 +0330

    Update extract_wav2vec.py

--- extract_feature/extract_wav2vec.py
+++ extract_feature/extract_wav2vec.py
@@ -4,7 +4,10 @@ import soundfile
 import torch
 import numpy as np
 import scipy.signal as signal
-from fairseq.models.wav2vec import Wav2VecModel
+# from fairseq.models.wav2vec import Wav2VecModel
+import fairseq
+import os
+from glob import glob
 
 def extract_wav2vec(wavfile, savefile):
     '''
@@ -37,16 +40,27 @@ if __name__ == '__main__':
     Pre-trained wav2vec model is available at https://github.com/pytorch/fairseq/blob/main/examples/wav2vec.
     Download model and save at model_path.
     '''
-    model_path = '/148Dataset/data-chen.weidong/pre_trained_model/wav2vec/wav2vec_large.pt'
+    model_path = './pre_trained_model/wav2vec/wav2vec_large.pt'
     sample_rate = 16000    # input should be resampled to 16kHz!
-    cp = torch.load(model_path, map_location='cpu')
-    wav2vec = Wav2VecModel.build_model(cp['args'], task=None)
-    wav2vec.load_state_dict(cp['model'])
+    # cp = torch.load(model_path, map_location='cpu')
+    # wav2vec = Wav2VecModel.build_model(cp['args'], task=None)
+    # wav2vec.load_state_dict(cp['model'])
+
+    model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([model_path])
+    wav2vec = model[0]
+    
     wav2vec.eval()
 
     #### use extract_wav2vec
-    wavfile = "./SpeechFormer/metadata/dataset/data/"
-    savefile = "./SpeechFormer/metadata/dataset/features/"
-    extract_wav2vec(wavfile, savefile)
+    wavfile = "metadata/dataset/data/content/selected_audio/"
+    savefile = "metadata/dataset/features/"
+    os.makedirs(savefile, exist_ok=True)
+
+    for f in glob(wavfile + '/*.wav'):
+      file_name = os.path.split(f)[-1].split('_')[:-1]
+      file_name = '_'.join(file_name)
+      file_name = os.path.join(savefile, file_name+'.mat')
+
+      extract_wav2vec(f, file_name)
     
     
